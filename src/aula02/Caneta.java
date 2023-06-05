8a2440427ba734f9a729838823534a230212c62a
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-06-04 20:33:39 -0400

    a

---
+++ src/aula02/Caneta.java
@@ -0,0 +1,31 @@
+package aula02;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean estaTampada;
+    public void status(){
+        System.out.println("A caneta é da cor " + this.cor);
+        System.out.println("O modelo é " + this.modelo);
+        System.out.println("A ponta é " + this.ponta);
+        System.out.println("A carga é " + this.carga);
+        System.out.println("Está tampada  " + this.estaTampada);
+    }
+    public String rabiscar(){
+        if (this.estaTampada){
+            return "Não pode rabiscar";
+        }
+        return "Estou rabiscando";
+    }
+
+    public void tamparDestanpar(){
+        this.estaTampada = !this.estaTampada;
+        if (this.estaTampada){
+            System.out.println("A caneta está tampada");
+        }else {
+            System.out.println("A caneta esta destampada");
+        }
+    }
+}
